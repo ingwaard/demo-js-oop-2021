6af146c32600ff39aa1a297801a6502611780121
ingwaard <igornyk@gmail.com>
2021-12-31 22:34:42 +0200

    objects

---
+++ main.js
@@ -0,0 +1,32 @@
+//Task 1 HW on Objects 1 Confectionery: Chocolates
+
+function Chocolate(){
+    const tmp = {}
+    tmp.base = ' '
+    tmp.filling = ' '
+    tmp.shape = ' '
+    tmp.price = 0
+    return tmp;
+    }
+
+const chocolate1 = Chocolate()
+chocolate1.base = 'dark chocolate';
+chocolate1.filling = 'liqueur';
+chocolate1.shape = 'heart';
+chocolate1.price = 20;
+
+const chocolate2 = Chocolate()
+chocolate2.base = 'milk chocolate';
+chocolate2.filling = 'souffle';
+chocolate2.shape = 'diamond';
+chocolate2.price = 15
+
+console.log(chocolate1);
+console.log(chocolate2);
+
+console.log(Chocolate());
+
+
+
+
+
